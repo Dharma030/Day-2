9a536faeacaf14b6ef314395e768fedce3f2ee83
Dharma030 <borntowindharma@gmail.com>
2023-11-12 08:40:55 +0530

    new commit1

--- server.js
+++ server.js
@@ -1,64 +1,54 @@
-const express = require('express');
+const express = require("express");
 const fs = require("fs");
 const path = require("path");
+
 const app = express();
 
-const outputFolder = "./output"; // Folder name where files will be stored
+const outputFolder = "./output"; //Folder name where files will be stored
 
-// To check whether the folder is already there, if not create a new folder
+//To check whether folder is alreadt there, if not create a new folder
 if (!fs.existsSync(outputFolder)) {
-    fs.mkdirSync(outputFolder);
+  fs.mkdirSync(outputFolder);
 }
 
 const PORT = 3000;
 
-app.get('/createFile', (req, res) => {
-    const currentTime = new Date();
-    const year = currentTime.getFullYear().toString();
-    const month = (currentTime.getMonth()+1).toString();
-    const date = currentTime.getDate().toString();
-    const hrs = currentTime.getHours().toString();
-    const mins = currentTime.getMinutes().toString();
-    const secs = currentTime.getSeconds().toString();
-    
-    const dateTimeForFileName = `${year}-${date}-${hrs}-${mins}-${secs}.txt`;
-    const filePath = path.join(outputFolder,dateTimeForFileName);
+app.get("/createFile", (req, res) => {
+  const currentTime = new Date();
+  const year = currentTime.getFullYear().toString();
+  const month = (currentTime.getMonth() + 1).toString();
+  const date = currentTime.getDate().toString();
+  const hrs = currentTime.getHours().toString();
+  const mins = currentTime.getMinutes().toString();
+  const secs = currentTime.getSeconds().toString();
 
-    fs.writeFile(filePath, currentTime.toISOString(),(err) => {
-        if(err){
-            res.status(500).send('Error creating file:${err}');
-            return;
-        }
+  const dateTimeForFileName = `${year}-${month}-${date}-${hrs}-${mins}-${secs}.txt`;
 
-        res.send('File created successfully at: ${filePath}');
-    })
+  const filePath = path.join(outputFolder, dateTimeForFileName);
 
+  fs.writeFile(filePath, currentTime.toISOString(), (err) => {
+    if (err) {
+      res.status(500).send(`Error creating file: ${err}`);
+      return;
+    }
 
-    // // Create a file name based on the current date and time
-    // const fileName = `${currentTime.getFullYear()}-${(
-    //     currentTime.getMonth() + 1)}-${currentTime.getDate()}-${currentTime.getHours()}-${currentTime.getMinutes()}-${currentTime.getSeconds()}.txt`;
-    
-    // // Create a sample content for the file
-    // const fileContent = "This is the content of the file.";
-    
-    // // Write the content to the file
-    // fs.writeFileSync(path.join(outputFolder, fileName), fileContent);
-    
-    // res.send(`File created with the name: ${fileName}`);
+    res.send(`File created successfully at: ${filePath}`);
+  });
 });
 
-app.get('/getFiles',(req, res) => {
-    fs.readdir(outputFolder, (err, files) =>{
-        if(err){
-            res.status(500).send(`Error reading files:${err}`);
-            return;
-        }
-        const textFiles = files.filter(file => path.extname(file) == ".txt");
-        res.json(textFiles);
-    });
+app.get("/getFiles", (req, res) => {
+  fs.readdir(outputFolder, (err, files) => {
+    if (err) {
+      res.status(500).send(`Error reading files: ${err}`);
+      return;
+    }
+    console.log("List of files:", files);
+    const textFiles = files.filter((file) => path.extname(file) === ".txt");
+
+    res.json(textFiles);
+  });
 });
 
 app.listen(PORT, () => {
-    
-    console.log("Server is running on PORT", PORT);
+  console.log("Server is running on PORT ", PORT);
 });
